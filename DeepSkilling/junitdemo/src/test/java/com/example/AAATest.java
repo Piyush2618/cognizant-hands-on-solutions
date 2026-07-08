74c96573010964d8d068baf6842dbb72ff012f5e
piyushpachauri <piyushpachauri3@gmail.com>
2026-07-08 14:12:32 +0530

    chore: completed exercise04 of AAA  in  JUnit

---
+++ DeepSkilling/junitdemo/src/test/java/com/example/AAATest.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AAATest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup executed");
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testSubtraction() {
+
+        
+        int a = 30;
+        int b = 20;
+
+        
+        int result = calculator.sub(a, b);
+
+     
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testSubtraction2() {
+
+        
+        int a = 50;
+        int b = 25;
+
+       
+        int result = calculator.sub(a, b);
+
+        
+        assertEquals(25, result);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("method executed");
+        calculator = null;
+    }
+}
