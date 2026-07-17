73853505ff2a73e39c07836c3ccc773324853cbb
piyushpachauri <piyushpachauri3@gmail.com>
2026-07-17 12:35:32 +0530

    chore: exercise02 Hello World RESTful Web Service of spring rest using springboot3 completed

--- DeepSkilling/Spring_REST_using_SpringBoot3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ DeepSkilling/Spring_REST_using_SpringBoot3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,31 +1,13 @@
 package com.cognizant.spring_learn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringLearnApplication {
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(SpringLearnApplication.class);
-
     public static void main(String[] args) {
-
-        display();
-
-    }
-
-    public static void display() {
-
-        ApplicationContext con =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        Country coun =
-                con.getBean("country", Country.class);
-
-       System.out.println("Country : " + coun);
-
+        SpringApplication.run(SpringLearnApplication.class, args);
     }
 
 }
