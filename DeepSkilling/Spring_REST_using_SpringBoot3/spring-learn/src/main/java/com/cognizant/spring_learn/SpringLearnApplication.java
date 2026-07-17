3aee58b075b2536b5fb012db215f09de61761923
piyushpachauri <piyushpachauri3@gmail.com>
2026-07-17 10:06:58 +0530

    chore: exercise01 hands on 4 of spring rest using springboot3 completed

--- DeepSkilling/Spring_REST_using_SpringBoot3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ DeepSkilling/Spring_REST_using_SpringBoot3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,31 @@
 package com.cognizant.spring_learn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-	}
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+
+        display();
+
+    }
+
+    public static void display() {
+
+        ApplicationContext con =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country coun =
+                con.getBean("country", Country.class);
+
+       System.out.println("Country : " + coun);
+
+    }
 
 }
